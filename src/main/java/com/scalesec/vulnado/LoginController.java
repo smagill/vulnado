3c55fdfe3362f4d051a98390a91774367da60e1f
Matt Bauer <mbauer@galois.com>
2020-06-10 11:01:04 -0400

    detect sql injection attack

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -6,39 +6,55 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.beans.factory.annotation.*;
+
 import java.io.Serializable;
+import java.util.Objects;
 
 @RestController
 @EnableAutoConfiguration
 public class LoginController {
-  @Value("${app.secret}")
-  private String secret;
+    @Value("${app.secret}")
+    private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-  LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
-      return new LoginResponse(user.token(secret));
-    } else {
-      throw new Unauthorized("Access Denied");
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    LoginResponse login(@RequestBody LoginRequest input) {
+
+        input = validateLoginRequest(input);
+
+        User user = User.fetch(input.username);
+        if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+            return new LoginResponse(user.token("dd"));
+        } else {
+            throw new Unauthorized("Access Denied");
+        }
     }
-  }
+
+    private LoginRequest validateLoginRequest(LoginRequest input) {
+        Objects.requireNonNull(input.username);
+        Objects.requireNonNull(input.password);
+
+        return input;
+    }
+
 }
 
 class LoginRequest implements Serializable {
-  public String username;
-  public String password;
+    public String username;
+    public String password;
 }
 
 class LoginResponse implements Serializable {
-  public String token;
-  public LoginResponse(String msg) { this.token = msg; }
+    public String token;
+
+    public LoginResponse(String msg) {
+        this.token = msg;
+    }
 }
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 class Unauthorized extends RuntimeException {
-  public Unauthorized(String exception) {
-    super(exception);
-  }
+    public Unauthorized(String exception) {
+        super(exception);
+    }
 }
